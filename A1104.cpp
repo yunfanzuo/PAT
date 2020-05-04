5dcc38b6f46a5dc7408cdae08069d096537cf590
ZuoYunF <975287514@qq.com>
2020-05-04 10:11:44 +0800

    easy

---
+++ A1104.cpp
@@ -0,0 +1,40 @@
+/*找到规律即可，在总和中，每个数出现次数为
+1：n * 1
+2: (n-1) * 2
+3: (n-2) * 3
+...
+n : 1 * n
+以此类推
+*/
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+const int maxn = 1e5 + 7;
+double a[maxn];
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++){
+        scanf("%lf", &a[i]);
+    }
+    double ans = 0.0;
+    // for(int i = 0; i < n; i++){
+    //     for(int j = 0; j < n - i; j++){
+    //         for(int k = i; k <= i + j; k++){
+    //             ans += a[k];
+    //         }
+    //     }
+    // }
+    int x = 1, y = n;
+    for(int i = 0; i < n; i++){
+        ans += a[i] * x * y;
+        x++;
+        y--;
+    }
+    printf("%.2f\n", ans);
+    return 0;
+}
